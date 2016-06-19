e717db7abc7b4aa13d46cacbeeef78a3a16a9892
matchalklen <mathewchalklen@gmail.com>
2016-06-19 16:48:37 +1000

    Java Script
    -Added java script index
    -Completed js/ jq to complete assignment objective
    -Need to clarify 2 column layout with instructors

---
+++ js/index.js
@@ -0,0 +1,38 @@
+function learnLess() {
+	$(".learnless").hide();
+	$("#learnmoretext").slideUp();
+	$(".learnmore").show();
+	event.preventDefault();
+}
+
+function learnMore() {
+	$(".learnmore").hide();
+	$("#learnmoretext").slideDown();
+	$(".learnless").show();
+	event.preventDefault();
+}
+
+function showLess() {
+	$(".readless").hide();
+	$("#show-this-on-click").slideUp();
+	$(".readmore").show();
+	event.preventDefault();
+}
+
+function showMore() {
+	$(".readmore").hide();
+	$("#show-this-on-click").slideDown();
+	$(".readless").show();
+	event.preventDefault();
+}
+
+function start() {
+	$(".readless").hide();
+	$(".learnless").hide();
+	$(".readmore").click(showMore);
+	$(".readless").click(showLess);
+	$(".learnmore").click(learnMore);
+	$(".learnless").click(learnLess)
+}
+
+$(document).ready(start);
